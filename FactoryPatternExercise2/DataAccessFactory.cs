395d0613857afe3e19a53127fdd6923a6c56fb27
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-18 14:22:36 -0600

    exercise complete

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2;
+
+internal static class DataAccessFactory
+{
+    public static IDataAccess GetDataAccessType(string databaseType)
+    {
+        switch (databaseType.ToLower())
+        {
+            case "mongo":
+                return new MongoDataAccess();
+            case "list":
+                return new ListDataAccess();
+            case "sql":
+                return new SQLDataAccess();
+            default:
+                return new ListDataAccess();
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace FactoryPatternExercise2
+﻿namespace FactoryPatternExercise2;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.Write("Select database to use (\"List\"(default), \"SQL\", or \"Mongo\"): ");
+        IDataAccess dataAccess = DataAccessFactory.GetDataAccessType(Console.ReadLine());
+
+        dataAccess.LoadData();
+        dataAccess.SaveData();
     }
 }
